b8512eb256c3b61e47e1ae893a3c724ceffe23e4
PirieD704 <dp75975@gmail.com>
2017-02-07 10:16:38 -0500

    basic functionality of carousel in place

---
+++ Part 1/js/script.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({
+  items: 2,
+  responsive : {
+      // breakpoint from 0 up
+      0 : {
+          items: 1
+      },
+      // breakpoint from 768 up
+      768 : {
+          items: 2
+      }
+  }
+})
